dc6a9c165fedd4ee31021e60e1ad00590b6d3bf0
dong uk <gusrn1255@naver.com>
2021-02-20 11:39:55 +0900

    head styled #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Router from "./Components/Router";
-
+import GlobalStyles from "./Components/GlobalStyles";
 const App = () => {
     return (
         <>
 
             <Router />
+            <GlobalStyles />
         </>
 
     );
